4e34e1bdd7114f0790ca9f542c87f4732de9e2b2
sobremesa <laroguf@hotmail.com>
2023-06-24 20:36:49 +0200

    Objetos sencillos

---
+++ src/main/java/org/rgil/ObjetosSencillos.java
@@ -0,0 +1,18 @@
+package org.rgil;
+
+public class ObjetosSencillos {
+    public static void main(String[] args) {
+        //Objetos booleanos
+        Boolean activo;
+        activo = true;
+        System.out.println(activo);
+
+        //Cadena de caracteres
+        String cadena ="Hola chat";
+        System.out.println(cadena);
+
+        //Acceder a propiedades del objeto
+        System.out.println(cadena.length());
+
+    }
+}
