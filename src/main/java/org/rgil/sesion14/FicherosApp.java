a42985f95a0fb3392285f3c953a9c1a675a1227e
sobremesa <laroguf@hotmail.com>
2023-07-21 17:50:24 +0200

    Manejo de Ficheros

--- src/main/java/org/rgil/sesion14/FicherosApp.java
+++ src/main/java/org/rgil/sesion14/FicherosApp.java
@@ -1,6 +1,8 @@
 package org.rgil.sesion14;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FicherosApp {
@@ -10,6 +12,7 @@ public class FicherosApp {
         File file = null;
         try{
             //Construimos el objeto de la Clase File para indicar esa ruta
+            //Este archivo se crea dentro de la carpeta principal del proyecto
             file = new File("./md5sum.txt");
             if (file.createNewFile()) {
                 System.out.println("Archivo creado");
@@ -20,5 +23,25 @@ public class FicherosApp {
             ioe.printStackTrace();
             System.out.println(ioe.getMessage());
         }
+
+        // Instrucciones para escribir en el fichero
+        BufferedWriter out = null;
+        try {
+            //Intento de escritura en el fichero
+            FileWriter fileWriter = new FileWriter("./md5sum.txt");
+            out = new BufferedWriter(fileWriter);
+            //Escribimos lo que queremos guardar
+            out.write("aString");
+            out.close();
+            System.out.println("Archivo creado con exito");
+        }catch (IOException e){
+            System.out.println("Fallo al escribir el fichero");
+            System.out.println(e.getMessage());
+        }// Se ejecuta siempre, haya pasado o no por el catch
+        finally {
+            System.out.println("Ejecutado en ambos casos try completo o catch");
+        }
+
+
     }
 }
